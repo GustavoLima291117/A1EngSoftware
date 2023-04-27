b861e48628adcdb0da623d1dfa8517346fd54634
Gustavo <gustavo.wervalska.lima@gmail.com>
2023-04-27 02:32:57 -0300

    first commit

---
+++ Transacoes.java
@@ -0,0 +1,27 @@
+package application;
+
+public class Transacoes {
+
+    private double valor;
+    private Conta contaRemetente;
+    private Conta contaDestinatario;
+
+    public Transacoes(double valor, Conta contaRemetente, Conta contaDestinatario) {
+        this.valor = valor;
+        this.contaRemetente = contaRemetente;
+        this.contaDestinatario = contaDestinatario;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Conta getContaRemetente() {
+        return contaRemetente;
+    }
+
+    public Conta getContaDestinatario() {
+        return contaDestinatario;
+    }
+
+}
